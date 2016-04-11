9e76bb6124f8bebb89dfcbc86b1ffe7a1ed7a816
Jovin Cronin-Wilesmith <jovin@stem.is>
2016-04-10 23:07:03 -0400

    Smooth Scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,19 @@
 var today = new Date();
 var todayYear = today.getFullYear();
 document.getElementById("year").innerHTML = todayYear;
+
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
